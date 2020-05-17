edec32cc2a3f45f91c63b230f8cfe2be734ef680
ksrawr <kenneth.surban62@gmail.com>
2020-05-17 01:27:07 -0700

    creates Hero Component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = props => {
+  return (
+    <div class="hero">
+      <p class="hero__greeting">WELCOME TO</p>
+      <p class="hero__title">Rooster Grin</p>
+      <button class="btn btn-primary hero__btn">BUTTON BUTTON</button>
+    </div>
+  )
+}
+
+export default Hero;
