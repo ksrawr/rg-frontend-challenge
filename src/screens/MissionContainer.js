9778c372aaedea939546a08f9c8f3c54552bdc04
ksrawr <kenneth.surban62@gmail.com>
2020-05-17 22:33:51 -0700

    adds mission container component with responsiveness

--- src/screens/MissionContainer.js
+++ src/screens/MissionContainer.js
@@ -5,20 +5,22 @@ import MissionContent from './../components/MissionContent/MissionContent';
 
 const MissionContainer = props => {
   return (
-    <div className="container container__mission">
-      <div className="row">
+    <div className="mission__section">
+    <div className="container-fluid">
+      <div className="row row__mission">
 
         {/* Mission Content */}
-        <div className="col-md-3 offset-md-3">
+        <div className="col-lg-3 offset-lg-3 col-md-3 offset-md-3 col-sm-12 col-xs-12 mission__content ml-auto">
           <MissionContent/>
         </div>
 
         {/* Mission Image */}
-        <div className="col-md-6">
-
+        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 ml-auto mt-2">
+          <img src="./../images/img-1.jpg" className="img-fluid"></img>
         </div>
 
       </div>
+    </div>
     </div>
   )
 };
