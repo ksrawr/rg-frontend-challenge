a5eab2d8029fe616f97dda0ccfece6da29d80c51
ksrawr <kenneth.surban62@gmail.com>
2020-05-17 20:24:36 -0700

    adds responvieness to hero container

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = props => {
   return (
-    <div className="hero">
+    <div className="hero col-md-12 col-sm-12 col-xs-12">
       <p className="hero__greeting">WELCOME TO</p>
       <p className="hero__title">Rooster Grin</p>
       <button className="btn__rg btn__rg--shadow btn__rg--purple hero__btn"><span className="btn__rg__text">BUTTON BUTTON</span></button>
