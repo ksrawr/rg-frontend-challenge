d9587637deea71ef7607f02e6303e5d829c12511
ksrawr <kenneth.surban62@gmail.com>
2020-05-18 02:30:14 -0700

    adds styling and layout to gettingstarted-comp

--- src/screens/GettingStartedContainer.js
+++ src/screens/GettingStartedContainer.js
@@ -5,9 +5,9 @@ const GettingStartedContainer = props => {
     <div className="gettingstarted__section">
       <div className="container-fluid">
         <div className="row row__gettingstarted">
-          <div className="col-lg-3 offset-lg-3 col-md-3 offset-md-3 col-sm-12 col-xs-12">
-            <h3 className="title title--purple">LOREM IPSUM</h3>
-            <button className="btn__rg--large btn__rg--purple">BUTTON BUTTON</button>
+        <div className="col-lg-3 offset-lg-2 col-md-12 col-sm-12 col-xs-12">
+            <h3 className="title title--purple mb-4">LOREM IPSUM</h3>
+            <button className="btn__rg--large btn__rg--orange"><span className="btn__rg--text">BUTTON BUTTON</span></button>
           </div>
         </div>
       </div>
