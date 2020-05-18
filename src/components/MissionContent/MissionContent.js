fb5e1cf44bc9a8a266b407ccdfa9eeb660092a3e
ksrawr <kenneth.surban62@gmail.com>
2020-05-18 01:50:18 -0700

    adds responsive to mission-content and mission-image

--- src/components/MissionContent/MissionContent.js
+++ src/components/MissionContent/MissionContent.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MissionContent = props => {
   return (
     <>
-    <h3 className="mc__title mc__title--purple mb-4">LOREM IPSUM</h3>
+    <h3 className="title title--purple mb-4">LOREM IPSUM</h3>
     <p className="mc__description">
       Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quisque risus vitae semper duis feugiat .
     </p>
