6be713cc4c94e4f88e441924e219be5b7e8708c5
ksrawr <kenneth.surban62@gmail.com>
2020-05-19 16:18:16 -0700

    adds responsiveness for mission and hero containers

--- src/components/MissionContent/MissionContent.js
+++ src/components/MissionContent/MissionContent.js
@@ -3,11 +3,23 @@ import React from 'react';
 const MissionContent = () => {
   return (
     <>
-    <h3 className="title title--purple mb-4">LOREM IPSUM</h3>
-    <p className="mc__description">
-      Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quisque risus vitae semper duis feugiat .
-    </p>
-    <a href="#" className="mc__link">&gt; Lorem Ipsum</a>
+    <div className="row">
+      <div className="col-lg-12">
+        <h3 className="title title--purple mb-4">LOREM IPSUM</h3>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-lg-12">
+        <p className="mc__description">
+          Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quisque risus vitae semper duis feugiat .
+        </p>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-lg-12">
+        <a href="#" className="mc__link">&gt; Lorem Ipsum</a>
+      </div>
+    </div>
     </>
   )
 };
--- src/screens/MissionContainer.js
+++ src/screens/MissionContainer.js
@@ -10,12 +10,12 @@ const MissionContainer = () => {
       <div className="row row__mission">
 
         {/* Mission Content */}
-        <div className="col-lg-3 offset-lg-2 col-md-12 col-sm-12 col-xs-12 mission__content">
+        <div className="col-lg-3 offset-lg-2 col-md-5 col-sm-12 col-xs-12 mission__content">
           <MissionContent/>
         </div>
 
         {/* Mission Image */}
-        <div className="col-lg-6 offset-lg-1 col-md-12 col-sm-12 col-xs-12 mt-2 mission__message">
+        <div className="col-lg-6 offset-lg-1 col-md-6 offset-md-1 col-sm-12 col-xs-12 mt-2 mission__message">
           <img src="./../images/img-1.jpg" className="img-fluid"></img>
         </div>
 
