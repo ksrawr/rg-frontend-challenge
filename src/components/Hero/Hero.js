586068de2aaeca17bcffc5358304bce4141fc8fe
ksrawr <kenneth.surban62@gmail.com>
2020-05-19 12:32:01 -0700

    adds fade in effect for herotitle

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className="hero col-md-12 col-sm-12 col-xs-12">
-      <p className="hero__greeting">WELCOME TO</p>
-      <p className="hero__title">Rooster Grin</p>
+      <div className="hero__title">
+        <p className="hero__greeting">WELCOME TO</p>
+        <p className="hero__name">Rooster Grin</p>
+      </div>
       <button className="btn__rg btn__rg--shadow btn__rg--purple hero__btn"><span className="btn__rg--text">BUTTON BUTTON</span></button>
     </div>
   )
