714fc5b53a34bd158a47f485af8491f1a0427012
ksrawr <kenneth.surban62@gmail.com>
2020-05-19 12:01:49 -0700

    adds animations to icons

--- src/components/OfferGrid/OfferGrid.js
+++ src/components/OfferGrid/OfferGrid.js
@@ -3,25 +3,25 @@ import React from 'react';
 const OfferGrid = props => {
   return(
     <div className="row row-cols-2">
-      <div className="col offer__icon--center mb-5">
+      <div className="col offer__icon--center offer__icon--animate mb-5">
         <div className="offer__icon--border">
           <div className="offer__icon offer__icon--clipboard"></div>
         </div>
         <p className="offer__icon__text">LOREM</p>
       </div>
-      <div className="col offer__icon--center mb-5">
+      <div className="col offer__icon--center offer__icon--animate mb-5">
         <div className="offer__icon--border">
           <div className="offer__icon offer__icon--facemask"></div>
         </div>
         <p className="offer__icon__text">LOREM IPSUM</p>
       </div>
-      <div className="col offer__icon--center">
+      <div className="col offer__icon--center offer__icon--animate">
         <div className="offer__icon--border">
           <div className="offer__icon offer__icon--lab"></div>
         </div>
         <p className="offer__icon__text">LOREM IPSUM</p>
       </div>
-      <div className="col offer__icon--center">
+      <div className="col offer__icon--center offer__icon--animate">
         <div className="offer__icon--border">
           <div className="offer__icon offer__icon--calendar"></div>
         </div>
